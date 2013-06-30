c3edab5e225adc96dce9974bfd0eb60597c32356
Michael LaCroix <michael@lacroixdesign.net>
2013-06-30 15:16:41 -0500

    begin integrating standard layout

--- test/helper_test.js
+++ test/helper_test.js
@@ -4,18 +4,18 @@ var app = require('../server.js');
 
 describe('app.locals', function(){
 
-  describe('#fullTitle()', function(){
+  describe('#pageTitle()', function(){
 
     it('should work when passed [string, string]', function(){
       // console.log(app.settings.env);
-      var title = app.locals.fullTitle('Page Title', 'Site Title');
+      var title = app.locals.pageTitle('Page Title', 'Site Title');
       assert.equal(title, 'Page Title | Site Title');
       // assert.equal(-1, [1,2,3].indexOf(5));
       // assert.equal(-1, [1,2,3].indexOf(0));
     });
 
     it('should work when passed [array, string]', function(){
-      var title = app.locals.fullTitle(['Page', 'Title'], 'Site Title');
+      var title = app.locals.pageTitle(['Page', 'Title'], 'Site Title');
       assert.equal(title, 'Page - Title | Site Title');
     });
 
