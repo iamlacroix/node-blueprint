077e96c434b645bef4f6e4c155b75317bced3784
Michael LaCroix <michael@lacroixdesign.net>
2013-09-15 05:08:42 -0500

    switch to new npm versions of boarding-pass, neat, & bourbon

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -6,7 +6,8 @@ module.exports = {
 
   dist: {
     options: {
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
+      includePaths: require('boarding-pass').includePaths
     },
     files: {
       '<%= meta.buildPath + meta.cssPath %>application.css': '<%= meta.sourcePath + meta.cssPath %>application.scss'
