46f024f11d2058017620b216b02bbd1d08ad9750
Michael LaCroix <michael@lacroixdesign.net>
2013-09-20 00:46:18 -0500

    use node clusters

--- server.js
+++ server.js
@@ -1,68 +1,78 @@
-// new relic
-if(process.env.NEW_RELIC_LICENSE_KEY) {
-  require('newrelic');
-}
+var cluster = require('cluster')
+  , cores   = process.env.CPU_CORES || 4;
 
-var express  = require('express')
-  , http     = require('http')
-  , path     = require('path');
-  // , util     = require('util')
-  // , moment   = require('moment')
-  // , rack     = require('asset-rack')
-  // , mongoose = require('mongoose');
+if (cluster.isMaster) {
+  for (var i = 0; i < cores; i++)
+    cluster.fork();
+} else {
 
-var app = module.exports = express();
+  // new relic
+  if(process.env.NEW_RELIC_LICENSE_KEY) {
+    require('newrelic');
+  }
 
-// assets
-app.use(express.compress());
-require('./config/assets')(app);
+  var express  = require('express')
+    , http     = require('http')
+    , path     = require('path');
+    // , util     = require('util')
+    // , moment   = require('moment')
+    // , rack     = require('asset-rack')
+    // , mongoose = require('mongoose');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.cookieSession({ secret: '_nodejs_blueprint_b2685bd0cb02e1049a903e3359c3903e3bbe' }));
-app.use(express.csrf());
+  var app = module.exports = express();
 
-// csrf
-app.use(function(req, res, next){
-  res.locals.token = req.csrfToken();
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  next();
-});
+  // assets
+  app.use(express.compress());
+  require('./config/assets')(app);
 
-// config
-require('./config/env')(app, express);
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.cookieSession({ secret: '_nodejs_blueprint_b2685bd0cb02e1049a903e3359c3903e3bbe' }));
+  app.use(express.csrf());
 
-// include any custom middleware before this app.router
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// mongoose
-// app.db = mongoose;
-// app.db.connect(app.get('mongodb-uri'));
-
-// models
-// require('./config/models')(app, mongoose);
-
-// services
-// app.set('Service', require('./lib/services/service'));
-
-// global helpers
-require('./lib/helpers')(app);
-app.locals.errors  = {};
-app.locals.message = {};
-
-// routes
-require('./config/routes')(app);
-
-// start server
-if (!module.parent) {
-  http.createServer(app).listen(app.get('port'), function() {
-    console.log('Server listening on port ' + app.get('port'));
+  // csrf
+  app.use(function(req, res, next){
+    res.locals.token = req.csrfToken();
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    next();
   });
+
+  // config
+  require('./config/env')(app, express);
+
+  // include any custom middleware before this app.router
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // mongoose
+  // app.db = mongoose;
+  // app.db.connect(app.get('mongodb-uri'));
+
+  // models
+  // require('./config/models')(app, mongoose);
+
+  // services
+  // app.set('Service', require('./lib/services/service'));
+
+  // global helpers
+  require('./lib/helpers')(app);
+  app.locals.errors  = {};
+  app.locals.message = {};
+
+  // routes
+  require('./config/routes')(app);
+
+  // start server
+  if (!module.parent) {
+    http.createServer(app).listen(app.get('port'), function() {
+      console.log('Server listening on port ' + app.get('port'));
+    });
+  }
+
 }
